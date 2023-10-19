6ca85ae21ac6f8ec83b40468ffec53aa27aea9e3
Kgethego Ledwaba <97187036+kgethegodev@users.noreply.github.com>
2023-10-19 16:29:13 +0200

    [chore] repo setup

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class = "container">
+    Layout: default
+    <slot />
+  </div>
+</template>
+
+<style></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    '~/assets/scss/style.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "sass:math"; @import "@/assets/scss/setup/_all.scss";'
+        }
+      }
+    }
+  }
 })
